6b64b55c02008e289ffa83982fb13ec808b86f87
Joydeep Hans <144369315+JOY23072005@users.noreply.github.com>
2025-06-17 02:09:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import { BrowserRouter, Routes,Route } from "react-router-dom"
 import ScrollToHashElement from "./components/ScrollToHashElement"
-import emailjs from ".emailjs/browser"
+import emailjs from "@emailjs/browser"
 
 function App() {
   emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY); 
